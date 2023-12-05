c8413a4830dd246a98e6b474a3ea01822d79fd11
Jacob Frausto <jacob_frausto@brown.edu>
2023-12-05 10:24:31 -0800

    Increased complexity of ANN

--- src/models/ANN.py
+++ src/models/ANN.py
@@ -7,13 +7,19 @@ class ANN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(ANN, self).__init__()
         self.layer_1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.2)
-        self.layer_2 = nn.Linear(hidden_size, output_size)
-    
+        self.leaky_relu_1 = nn.LeakyReLU(0.2)
+        self.dropout_1 = nn.Dropout(0.2)
+        self.layer_2 = nn.Linear(hidden_size, hidden_size)
+        self.leaky_relu_2 = nn.LeakyReLU(0.2)
+        self.dropout_2 = nn.Dropout(0.2)
+        self.layer_3 = nn.Linear(hidden_size, output_size)
+
     def forward(self, x):
         x = self.layer_1(x)
-        x = self.relu(x)
-        x = self.dropout(x)
+        x = self.leaky_relu_1(x)
+        x = self.dropout_1(x)
         x = self.layer_2(x)
+        x = self.leaky_relu_2(x)
+        x = self.dropout_2(x)
+        x = self.layer_3(x)
         return x
